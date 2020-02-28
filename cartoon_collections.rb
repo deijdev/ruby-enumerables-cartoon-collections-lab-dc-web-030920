b8334c7f7c64cbf26fd1882af656d8241dd22b94
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 04:31:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
- def roll_call_dwarves(array)
-   array.each_with_index do |name, index|
+ def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
      puts "#{index+1}. #{name}"
    end 
  end 
