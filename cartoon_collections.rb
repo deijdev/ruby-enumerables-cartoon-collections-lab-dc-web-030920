951eacb4c0cae0df0d2b872b423c71ace287161b
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 04:14:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,7 @@
 def roll_call_dwarves(array)
   dwarve_names=["Doc", "Dopey", "Bashful", "Grumpy"]
-  num=0
-  dwarve_names.each {|n| 
-    puts %{num} + n
-  }
-  num += 1
-  # Your code here
-end
+ dwarve_names.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
+end	
 
 def summon_captain_planet# code an argument here
   # Your code here
