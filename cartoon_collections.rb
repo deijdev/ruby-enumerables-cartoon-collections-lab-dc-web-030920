aa3e26a2e293ca18bba9e5414fb0f55616fc43ed
Deijah Price <deijahwilson1@gmail.com>
2020-02-28 04:22:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,10 +7,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(short_words)
-  
-   str.count(n)> 4??
-  # Your code here
-end
+   planeteer_calls.any? do |call|
+  call.length > 4
+  end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
